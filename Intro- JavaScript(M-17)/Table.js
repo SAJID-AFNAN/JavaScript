35543ff452f36ee74457d13c4e255c1d32c87862
SAJID-AFNAN <sajidafnan91@gmail.com>
2024-12-25 22:58:53 +0600

    some changes

---
+++ Intro- JavaScript(M-17)/Table.js
@@ -0,0 +1,16 @@
+const apple = 350
+let orange = 270 
+var tomato = 100
+let me = 1000
+
+// apple = 300     //const variable is fixed. is not changeable
+
+/*
+Always use let,const, not use var .
+because var has scope issue
+*/
+
+console.table([apple,orange,tomato,me]);
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object
